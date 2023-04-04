dba9245bfb3aeff53f8b514896ae269486d35edc
Johan Steenhoven <1254233+sbkg0002@users.noreply.github.com>
2023-04-04 14:04:46 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 echo "##################################"
 echo "hard-fail checks: ${1}"
 echo "path: ${2}"
+echo "skip-checks: ${3}"
 echo "##################################"
 echo "running checkov:"
 echo "##################################"
-python3 /main.py ${1} ${2}
+python3 /main.py --hard-fail-on ${1} --path ${2} --skip-checks ${3}
