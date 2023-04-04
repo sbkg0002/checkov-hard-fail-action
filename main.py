9c1ef70946ce8a63eb130cc568436d7d541f82a4
Johan Steenhoven <1254233+sbkg0002@users.noreply.github.com>
2023-04-04 12:16:41 +0200

    Use action inputs

--- main.py
+++ main.py
@@ -15,22 +15,22 @@ def checkov(config_file: str, code_path: str) -> None:
         check=False,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
+    print(checkov_process.stdout)
     cli_output, json_output = checkov_process.stdout.split('--- OUTPUT DELIMITER ---')
 
     print(cli_output)
-    checkov_results = json.loads(json_output)[0]
-    # print(type(checkov_results))
+    checkov_results = json.loads(json_output)
 
     # open configfile to check for hard fail CKVs
     with open(config_file, 'r', encoding='utf-8') as config:
         checkov_config = yaml.safe_load(config)
 
-    if not (hard_fail_ids := checkov_config.get('hard-fail-on')):
+    if not (hard_fail_ids := sys.argv[1]):
         sys.exit(checkov_process.returncode)
 
     illegal_skip = False
     for skipped_check in checkov_results['results']['skipped_checks']:
-        if skipped_check['check_id'] not in hard_fail_ids:
+        if skipped_check['check_id'] not in hard_fail_ids.split(','):
             continue
         print(f"The following check cannot be skipped:\n"
               f"\tCheck: {skipped_check['check_id']}: {skipped_check['check_name']}\n"
@@ -44,4 +44,4 @@ def checkov(config_file: str, code_path: str) -> None:
 
 
 if __name__ == '__main__':
-    checkov(config_file='/config.yaml', code_path='/github/workspace/')
+    checkov(config_file='./config.yaml', code_path='/github/workspace/')
