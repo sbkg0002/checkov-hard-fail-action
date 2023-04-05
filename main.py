3fc9667c10ecfec7ee98bd1afc8ffddeebd05c8d
Johan Steenhoven <1254233+sbkg0002@users.noreply.github.com>
2023-04-05 10:12:57 +0200

    Fix output

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ args = parser.parse_args()
 def checkov(code_path: str) -> None:
     """Main process that checks for skipped checks against the list of hard fails"""
     checkov_process = subprocess.run(
-        ["checkov", "-o", "cli", "-o", "json", "-d", code_path, "--quiet",
-            "--download-external-modules", "True", "--framework", "terraform", "--skip-check", args.skip_checks],
+        ["checkov", "-o", "cli", "-o", "json", "-d", code_path,
+         "--download-external-modules", "True", "--framework", "terraform", "--skip-check", args.skip_checks],
         universal_newlines=True,
         check=False,
         stdout=subprocess.PIPE,
@@ -32,20 +32,25 @@ def checkov(code_path: str) -> None:
         sys.exit(checkov_process.returncode)
 
     illegal_skip = False
-    for skipped_check in checkov_results['results']['skipped_checks']:
-        if skipped_check['check_id'] not in hard_fail_ids.split(','):
-            continue
-        print(f"The following check cannot be skipped:\n"
-              f"\tCheck: {skipped_check['check_id']}: {skipped_check['check_name']}\n"
-              f"\tFile: {skipped_check['file_path']}")
-        illegal_skip = True
+    if checkov_results['summary'].get('skipped') > 0:
+        for skipped_check in checkov_results['results'].get('skipped_checks'):
+            if skipped_check['check_id'] not in hard_fail_ids.split(','):
+                continue
+            print(f"The following check cannot be skipped:\n"
+                  f"\tCheck: {skipped_check['check_id']}: {skipped_check['check_name']}\n"
+                  f"\tFile: {skipped_check['file_path']}")
+            illegal_skip = True
 
-    if illegal_skip:
-        print("\nThe terraform code that has been checked contains hard enforced checks,"
-              " it is not allowed to skip hard enforced check: see: http://someniceconfluence doc")
-        sys.exit(1)
+        if illegal_skip:
+            print("\n### MANDATORY CHECK(S) FAILED ###"
+                  "\nThe terraform code that has been checked contains hard enforced checks,"
+                  " it is not allowed to skip hard enforced checks. For more information see: "
+                  "\n\thttps://devolksbank.atlassian.net/wiki/spaces/CEP/pages/49762271982/Terraform+code+analysis")
+            sys.exit(1)
 
 
 if __name__ == '__main__':
     CODE_PATH = f'/github/workspace/{args.path}'
+    # CODE_PATH = f'/github/workspace/{args.path}'
+    CODE_PATH = '.'
     checkov(code_path=CODE_PATH)
